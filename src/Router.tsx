3510bdf8674ea1d1e98ed53d6cfe6f4c66483787
Salomé Gallego A <sgallegoa11@gmail.com>
2024-03-15 02:56:11 -0500

    Agregando ruta del registro

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
+import RegistroPage from "./pages/register";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegistroPage />} />
     </Routes>
   );
 };
