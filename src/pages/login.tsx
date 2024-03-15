96cc4cc9164d352fc04f48d68b5649ab38295ebc
Salomé Gallego A <sgallegoa11@gmail.com>
2024-03-15 02:28:46 -0500

    Organizando Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,7 +11,7 @@ import {
     Typography,
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import Logo from '../assets/logo_bia.png';
+import Logo from '../assets/logo_bia.png'; // Asegúrate de que la ruta del logo sea correcta
 
 const GradientDivider = styled('div')({
     height: '4px',
@@ -20,85 +20,110 @@ const GradientDivider = styled('div')({
     margin: '0 auto', // Centrar el divider horizontalmente
 });
 
+type LoginType = {
+    username: string;
+    password: string;
+};
+
 const LoginPage: React.FC = () => {
+    const [loginData, setLoginData] = React.useState<LoginType>({ username: "", password: "" });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(loginData);
+    };
+
     return (
         <Container component="main" sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}>
             <CssBaseline />
             <Paper elevation={6} sx={{
                 borderRadius: '16px',
-                width: 699, // Ancho del paper
-                height: 516, // Alto del paper
-                mx: 'auto', // Centrar el paper horizontalmente
+                width: 699,
+                height: 516,
+                mx: 'auto',
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
-                position: 'relative', // Para posicionar absolutamente el divider
-                overflow: 'hidden', // Para los bordes redondeados
+                position: 'relative',
             }}>
                 <Box sx={{ textAlign: 'center', my: 2 }}>
                     <img src={Logo} alt="Logo Bia" width="100" />
                 </Box>
                 <GradientDivider />
-                <Grid container spacing={2} sx={{ p: 3, width: '100%',}}>
-                    <Grid item xs={12} sm={6} container direction="column" alignItems="center" sx={{ gap: 1, mt:5, }}>
-                        <TextField
-                            label="Usuario"
-                            variant="outlined"
-                            fullWidth
-                            required
-                            size="small"
-                            sx={{ width: '80%', my: 1 }} // Ajuste de los márgenes superior e inferior
-                        />
-                        <TextField
-                            label="Contraseña"
-                            type="password"
-                            variant="outlined"
-                            fullWidth
-                            required
-                            size="small"
-                            sx={{ width: '80%', my: 1 }} // Ajuste de los márgenes superior e inferior
-                        />
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            sx={{
-                                mt: 1,
-                                width: '80%',
-                            }}
-                        >
-                            INICIAR SESIÓN
-                        </Button>
-                        <Link href="#" variant="body2" sx={{ mt: 1, width: '80%', display: 'flex', justifyContent: 'center' }}>
-                                ¿Olvidó su contraseña?
-                            </Link>
-                            <Link href="#" variant="body2" sx={{ mt: 1, width: '80%', display: 'flex', justifyContent: 'center' }}>
-                                ¿Olvidó su usuario?
-                            </Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper elevation={0} sx={{
-                            p: 2,
-                            borderRadius: '16px',
-                            backgroundColor: '#E3F2FD', // Un tono azul claro
-                            textAlign: 'center', // Centrar texto
-                            height: '100%', // Asegurar altura completa
-                        }}>
-                            <Typography variant="body2" gutterBottom>
-                                Bienvenido
-                            </Typography>
-                            <Typography variant="body2" gutterBottom>
-                                Este es nuestro portal de trámites en línea un sistema fácil de usar, ágil y sencillo para el control general de solicitudes y requerimientos ambientales en el departamento del Meta.
-                            </Typography>
-                            <Button variant="outlined" sx={{ mt: 3, borderColor: 'primary.dark', color: 'primary.dark' }}>
-                                REGÍSTRESE
+                <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                    <Grid container spacing={2} sx={{ p: 3 }}>
+                        <Grid item xs={12} sm={6} container direction="column" alignItems="center" sx={{ gap: 1, mt: 5 }}>
+                            <TextField
+                                label="Usuario"
+                                name="username"
+                                type="email"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                size="small"
+                                sx={{ width: '80%', my: 1 }}
+                                onChange={handleInputChange}
+                            />
+                            <TextField
+                                label="Contraseña"
+                                name="password"
+                                type="password"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                size="small"
+                                sx={{ width: '80%', my: 1 }}
+                                onChange={handleInputChange}
+                            />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                sx={{ mt: 1, width: '80%' }}
+                            >
+                                INICIAR SESIÓN
                             </Button>
-                            <Typography variant="body2" gutterBottom sx={{ mt: 3, fontStyle: 'italic' }}>
-                                Si tiene algún reclamo o solicitud, ingrese a PQR en línea Número de atención: Línea nacional 01-8000-51847095 Email: pqrds@cormacarena.gov.co
-                            </Typography>
-                        </Paper>
+                            <Box sx={{ mt: 1, width: '80%', textAlign: 'center' }}>
+                                <Link href="#" variant="body2">
+                                    ¿Olvidó su contraseña?
+                                </Link>
+                                <br />
+                                <Link href="#" variant="body2">
+                                    ¿Olvidó su usuario?
+                                </Link>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={6} sx={{ textAlign: 'center', p: 2 }}>
+                            <Paper elevation={0} sx={{
+                                p: 2,
+                                borderRadius: '16px',
+                                backgroundColor: '#E3F2FD', // Un tono azul claro
+                                height: '100%', // Asegurar altura completa
+                                display: 'flex', // Añadido para disponer los elementos internos en columna
+                                flexDirection: 'column', // Añadido para disponer los elementos internos en columna
+                                justifyContent: 'center', // Añadido para centrar verticalmente en caso de que sea necesario
+                            }}>
+                                <Typography variant="body2" gutterBottom>
+                                    Bienvenido
+                                </Typography>
+                                <Typography variant="body2" gutterBottom>
+                                    Este es nuestro portal de trámites en línea un sistema fácil de usar, ágil y sencillo para el control general de solicitudes y requerimientos ambientales en el departamento del Meta.
+                                </Typography>
+                                <Button variant="outlined" sx={{ mt: 3, borderColor: 'primary.dark', color: 'primary.dark' }}>
+                                    REGÍSTRESE
+                                </Button>
+                                <Typography variant="caption" sx={{ mt: 2, fontStyle: 'italic' }}>
+                                    Si tiene algún reclamo o solicitud, ingrese a PQR en línea Número de atención: Línea nacional 01-8000-51847095 Email: pqrds@cormacarena.gov.co
+                                </Typography>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </form>
             </Paper>
         </Container>
     );
